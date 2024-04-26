defb674df2cbe4c982a6d634a3e3537cc1aeba63
jarethholt <jareth.holt@gmail.com>
2024-04-26 15:56:26 +0200

    refactor: Change AddressTagHelper to directly use Address class

--- LexiconUniversity.Web/TagHelpers/AddressTagHelper.cs
+++ LexiconUniversity.Web/TagHelpers/AddressTagHelper.cs
@@ -1,18 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+﻿using LexiconUniversity.Core.Entities;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace LexiconUniversity.Web.TagHelpers;
 
 public class AddressTagHelper : TagHelper
 {
-    public string Street { get; set; } = string.Empty;
-    public string City { get; set; } = string.Empty;
-    public string ZipCode { get; set; } = string.Empty;
+    public Address Address { get; set; } = default!;
 
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
         output.TagName = "p";
         output.Attributes.SetAttribute("style", "white-space:pre-line");
-        output.Content.SetContent($"{Street}\n{ZipCode} {City}");
+        output.Content.SetContent($"{Address.Street}\n{Address.ZipCode} {Address.City}");
     }
 }
